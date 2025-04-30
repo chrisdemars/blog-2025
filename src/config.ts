89d2cb06e347a637b1a0e2202b45619c62f61a14
Chris DeMars <cdemars86@gmail.com>
2025-04-29 21:00:17 -0400

    deletes and adds a bunch of stuff

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Chris DeMars | Senior Developer Advocate';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_DESCRIPTION = 'I’m a tattooed Senior Developer Advocate based in Metro Detroit, passionate about frontend tech, storytelling, and building community. Outside of work, I’m a competitive bass angler, paintball player, and paranormal enthusiast chasing down UFOs, cryptids, and ghosts.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
